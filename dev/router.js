9a2650829057a1a2f441a16fe01fc4e4d25e893e
iapYang <yklorse@gmail.com>
2017-03-16 17:00:56 +0800

    0316 remove test

--- dev/router.js
+++ dev/router.js
@@ -1,16 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Test from './component/Test.vue';
-
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/test',
-        name: 'test',
-        component: Test,
-    },
+    // {
+    //     path: '/test',
+    //     name: 'test',
+    //     component: Test,
+    // },
 ];
 
 const router = new VueRouter({
