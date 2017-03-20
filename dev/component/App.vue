d711775bbd1ff4dd31e87222ce64507a39f1c5d4
iapYang <yklorse@gmail.com>
2017-03-20 11:31:20 +0800

    0320 UI change

--- dev/component/App.vue
+++ dev/component/App.vue
@@ -1,8 +1,33 @@
 <template lang="html">
     <div class="app-container">
-        <md-toolbar>
-            <h1 class="md-title">My Title</h1>
-        </md-toolbar>
+        <header>
+            <md-toolbar>
+                <h1 class="md-title">Vue-video-module</h1>
+            </md-toolbar>
+        </header>
+        <div class="banner">
+            <h2>Vue-video-module</h2>
+            <h4>Simple video component bulit for vue</h4>
+            <ul class="actions">
+                <li class="action" v-for="action,index in actions" :key="action">
+                    <a :href="action.href" target="_blank">
+                        <md-button :class="action.class">{{ action.name }}</md-button>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="examples container">
+            <md-card>
+                <md-card-header>
+                    <div class="md-subhead">Subtitle here</div>
+                </md-card-header>
+                <div class="md-card-main">
+                    <div class="video-container container">
+                        <vue-video ref="video1" :options="videoOptions"></vue-video>
+                    </div>
+                </div>
+            </md-card>
+        </div>
         <div class="video-container container">
             <vue-video ref="video1" :options="videoOptions"></vue-video>
         </div>
@@ -17,7 +42,7 @@
     import VueVideo from './VueVideo.vue';
     import VueMaterial from 'vue-material';
     import 'vue-material/dist/vue-material.css';
-
+    
     Vue.use(VueMaterial);
     
     import {
@@ -28,6 +53,19 @@
     export default {
         data() {
             return {
+                actions: [{
+                    name: 'view on github',
+                    href: 'https://github.com/iapYang/vue-video-module',
+                    class: 'md-primary',
+                }, {
+                    name: 'github issues',
+                    href: 'https://github.com/iapYang/vue-video-module/issues',
+                    class: 'md-accent',
+                }, {
+                    name: 'API',
+                    href: 'https://github.com/iapYang/vue-video-module/blob/master/README.md',
+                    class: 'md-primary',
+                }],
                 videoOptions: {
                     src: 'http://vjs.zencdn.net/v/oceans.mp4',
                     poster: 'http://www.freemake.com/blog/wp-content/uploads/2015/06/videojs-logo.jpg',
@@ -37,20 +75,20 @@
                     },
                 },
                 code: `// example
-        export default {
-            data() {
-                return {
-                    videoOptions: {
-                        src: 'http://vjs.zencdn.net/v/oceans.mp4',
-                        poster: 'http://www.freemake.com/blog/wp-content/uploads/2015/06/videojs-logo.jpg',
-                        fullscreen: true,
-                        onStart(vueVideo) {
-                            console.log(vueVideo);
-                        },
-                    },
-                };
-            },
-        };`,
+                            export default {
+                                data() {
+                                    return {
+                                        videoOptions: {
+                                            src: 'http://vjs.zencdn.net/v/oceans.mp4',
+                                            poster: 'http://www.freemake.com/blog/wp-content/uploads/2015/06/videojs-logo.jpg',
+                                            fullscreen: true,
+                                            onStart(vueVideo) {
+                                                console.log(vueVideo);
+                                            },
+                                        },
+                                    };
+                                },
+                            };`,
                 editorOption: {
                     tabSize: 4,
                     styleActiveLine: true,
@@ -107,12 +145,34 @@
         position: relative;
         width: 100%;
         height: 100%;
+        min-width: 768px;
     }
     
-    .title {
-        font-family: "Lato-Regular";
-        font-size: 50px;
-        text-align: center;
+    .banner {
+        h2 {
+            // font-family: "Lato-Regular";
+            text-transform: uppercase;
+            font-size: 35px;
+            text-align: center;
+            color: #000;
+            line-height: 1;
+        }
+        h4 {
+            // font-family: "Lato-Regular";
+            text-transform: capitalize;
+            font-size: 25px;
+            text-align: center;
+            color: #a5a5a5;
+            margin-bottom: 0;
+            line-height: 1;
+        }
+        .actions {
+            position: relative;
+            text-align: center;
+            li {
+                display: inline-block;
+            }
+        }
     }
     
     .container {
