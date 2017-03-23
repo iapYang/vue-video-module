3af76ef4ed214023559caa211a53779de5fbdf26
iapYang <yklorse@gmail.com>
2017-03-23 17:50:20 +0800

    add gh-pages

---
+++ conf/deploy.js
@@ -0,0 +1,10 @@
+const ghpages = require('gh-pages');
+const path = require('path');
+
+ghpages.publish(path.resolve(__dirname, '../docs'), err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('publish to github page done');
+    }
+});
