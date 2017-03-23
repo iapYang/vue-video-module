00223e8a8e3e380899831dfc83f9beeb6930f2ec
iapYang <yklorse@gmail.com>
2017-03-23 17:46:34 +0800

    Updates

---
+++ view/App.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <div class="app-container">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style lang="scss">
+    // @import "../style/reset";
+    @import "../style/mixin";
+    @include font-face("Lato-Regular");
+    html,
+    body {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        overflow-x: hidden;
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+    
+    #app,
+    .app-container {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        min-width: 768px;
+    }
+</style>
