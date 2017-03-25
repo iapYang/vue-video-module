0df418541710944ed69545265daac2634336ffdb
iapyang <yklorse@gmail.com>
2017-03-26 02:20:49 +0800

    update video

--- dev/view/Test.vue
+++ dev/view/Test.vue
@@ -1,7 +1,7 @@
 <template lang="html">
     <div class="test-container">
         <top :showButton="true"></top>
-        <h2>Video came from Youtube. Maybe you have to wait for a while.</h2>
+        <h2>Video came from The Lorax Forces of Nature</h2>
         <div class="test-main">
             <div class="field-group">
                 <md-input-container v-for="select,index in selects" key="select">
@@ -29,7 +29,7 @@
             const selects = [{
                 name: 'src',
                 options: [
-                    'https://redirector.googlevideo.com/videoplayback?lmt=1471566762181218&sparams=dur%2Cei%2Cgcr%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&ei=IjnWWJO0PNS5-APvgpOIDg&dur=544.577&signature=CE8B51D5558380FB78A40851449392CABCDBBA24.270EAFA75683352C622B73B62609C942E54C8412&initcwndbps=5095000&ipbits=0&ratebypass=yes&upn=vam_c1LfTIo&key=yt6&itag=22&mn=sn-n4v7sn7l&pl=33&source=youtube&mm=31&requiressl=yes&ip=2600%3A3c01%3A%3Af03c%3A91ff%3Afe24%3Ab564&mime=video%2Fmp4&expire=1490455939&mt=1490434226&ms=au&id=o-ADTYoua3b4FZWSdj0lFrJ598SO3-gMYlTnJn5aZeMph0&mv=m&gcr=us',
+                    'video/1.mp4',
                     'http://vjs.zencdn.net/v/oceans.mp4',
                 ],
             }, {
@@ -125,6 +125,7 @@
         .video-container {
             position: relative;
             width: 70%;
+            max-width: 1200px;
             margin: 0 auto;
         }
     }
