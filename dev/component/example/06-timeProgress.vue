7bd4cc2bfdd4ff8cac7a9f471b4b3121ebc849d1
iapyang <yklorse@gmail.com>
2017-09-07 11:35:03 +0800

    upload example

---
+++ dev/component/example/06-timeProgress.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+    <div class="video-container">
+        <vue-video ref="video1" :options="videoOptions"></vue-video>
+    </div>
+</template>
+
+<script>
+    import VueVideo from '../src/VueVideo.vue';
+
+    export default {
+        data() {
+            return {
+                videoOptions: {
+                    src: 'http://vjs.zencdn.net/v/oceans.mp4',
+                    timeProgress: true,
+                },
+            };
+        },
+        mounted() {
+            this.video1 = this.$refs.video1;
+        },
+        components: {
+            VueVideo,
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+    @import '../../style/_example';
+</style>
