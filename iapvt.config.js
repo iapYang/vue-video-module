d68040d91537a36948b9c0f60ac80961d6cb13ee
iapyang <yklorse@gmail.com>
2017-09-07 17:07:58 +0800

    update

--- iapvt.config.js
+++ iapvt.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     bundle: {
-        path: path.resolve(__dirname, './dev/component/src/index.js'),
+        path: path.resolve(__dirname, './dev/component/src/VueVideo.vue'),
         name: 'VueVideo',
     },
 };
